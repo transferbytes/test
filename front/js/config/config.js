40e2a0fada9f458c14d91635ec06b3c8e484eae9
Pau Obrador <pau.obrador@atrapalo.com>
2018-12-21 11:06:13 +0100

    [ADD] styles

---
+++ front/js/config/config.js
@@ -0,0 +1,13 @@
+﻿const config = {
+    infobook: 'ae22207d4c06c8efe705b0197e42b5d7b017b22ae7a71fb738c1f79f5bafcdc6',
+    descargaarchivo: 'e7bd38aeb8c872ed3032cf09624b24d1ad8a61de3774f6b90096f309a6f1d7c7',
+    nuevoemail: '70275140e583c498a2c5a3971187748f383ac37e9e8d81491b77d3b60fef340f',
+    comprobaremail: '1f31efa5197bae592a7608b399bc39466dbaaa1bab7a46a40077258703fc63af'
+};
+
+const  key = 'tb-ADFA0D0D43E89F0A146B4FC93811E9AB5D3033F24708DF4B1790A3227F0E1288';
+
+export {
+    config,
+    key
+};
--- front/js/pages/index/index.js
+++ front/js/pages/index/index.js
@@ -1,3 +1,5 @@
+import {config, key} from '../../config/config';
+
 new Vue({
     el: '#nuevoemail',
     data: {
@@ -24,8 +26,8 @@ new Vue({
                 this.sms = '';
 
                 transferBytes.connect({
-                    id: '70275140e583c498a2c5a3971187748f383ac37e9e8d81491b77d3b60fef340f',
-                    license: 'tb-ADFA0D0D43E89F0A146B4FC93811E9AB5D3033F24708DF4B1790A3227F0E1288',
+                    id: config.nuevoemail,
+                    license: key,
                     languaje: this.languaje,
                     content: information,
                     idPost: 0
@@ -41,8 +43,8 @@ new Vue({
 
         downloadBook() {
             transferBytes.connect({
-                id: 'e7bd38aeb8c872ed3032cf09624b24d1ad8a61de3774f6b90096f309a6f1d7c7',
-                license: 'tb-ADFA0D0D43E89F0A146B4FC93811E9AB5D3033F24708DF4B1790A3227F0E1288',
+                id: config.descargaarchivo,
+                license: key,
                 languaje: this.languaje,
                 content: ['Si'],
                 idPost: this.idPost
@@ -60,8 +62,8 @@ new Vue({
                 var information = [this.info_email];
 
                 transferBytes.connect({
-                    id: '1f31efa5197bae592a7608b399bc39466dbaaa1bab7a46a40077258703fc63af',
-                    license: 'tb-ADFA0D0D43E89F0A146B4FC93811E9AB5D3033F24708DF4B1790A3227F0E1288',
+                    id: config.comprobaremail,
+                    license: key,
                     languaje: this.languaje,
                     content: information,
                     idPost: 0
@@ -80,8 +82,8 @@ new Vue({
         infobook() {
             var information = [];
             transferBytes.connect({
-                id: 'ae22207d4c06c8efe705b0197e42b5d7b017b22ae7a71fb738c1f79f5bafcdc6',
-                license: 'tb-ADFA0D0D43E89F0A146B4FC93811E9AB5D3033F24708DF4B1790A3227F0E1288',
+                id: config.infobook,
+                license: key,
                 languaje: this.languaje,
                 content: information,
                 idPost: 0
@@ -89,7 +91,7 @@ new Vue({
                 this.href = `https://www.transferbytes.io/assets/src/files/${result[0].pdf[0]}`;
                 this.titulo = result[0].titulo;
                 this.texto = result[0].texto;
-                this.legal = result[0].legal;
+                this.legal = result[0].textoLegal;
             });
         },
 
@@ -98,7 +100,7 @@ new Vue({
 
             transferBytes.connect({
                 id: '1f31efa5197bae592a7608b399bc39466dbaaa1bab7a46a40077258703fc63af',
-                license: 'tb-ADFA0D0D43E89F0A146B4FC93811E9AB5D3033F24708DF4B1790A3227F0E1288',
+                license: key,
                 languaje: this.languaje,
                 content: information,
                 idPost: 0
